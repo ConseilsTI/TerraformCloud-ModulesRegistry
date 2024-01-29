c7ac86dd24d6d4461f8e6c202df9ba6392a0c69a
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-01-29 13:07:29 -0500

    v1.0.0

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 # The following code block is used to create GitHub team.
 
 resource "github_team" "this" {
-  name                      = var.team_name
-  description               = var.team_description
-  privacy                   = "closed"
+  name        = var.team_name
+  description = var.team_description
+  privacy     = "closed"
 }
 
 # The following code block is used to create GitHub repository.
