f948fc76dc9009a8c6a94b94001ebc337f237344
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-01-30 11:37:30 -0500

    v1.0.1

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,51 @@
 output "repository" {
   description = "Repositories within your GitHub organization."
-  value       = github_repository.this
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository }
 }
 
 output "full_name" {
   description = "A string of the form \"orgname/reponame\"."
-  value       = github_repository.this.full_name
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.full_name }
 }
 
 output "html_url" {
   description = "URL to the repository on the web."
-  value       = github_repository.this.html_url
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.html_url }
 }
 
 output "ssh_clone_url" {
   description = "URL that can be provided to git clone to clone the repository via SSH."
-  value       = github_repository.this.ssh_clone_url
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.ssh_clone_url }
 }
 
 output "http_clone_url" {
   description = "URL that can be provided to git clone to clone the repository via HTTPS."
-  value       = github_repository.this.http_clone_url
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.http_clone_url }
 }
 
 output "git_clone_url" {
   description = "URL that can be provided to git clone to clone the repository anonymously via the git protocol."
-  value       = github_repository.this.git_clone_url
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.git_clone_url }
 }
 
 output "svn_url" {
   description = "URL that can be provided to svn checkout to check out the repository via GitHub's Subversion protocol emulation."
-  value       = github_repository.this.svn_url
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.svn_url }
 }
 
 output "node_id" {
   description = "GraphQL global node id for use with v4 API."
-  value       = github_repository.this.node_id
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.node_id }
 }
 
 output "repo_id" {
   description = "GitHub ID for the repository."
-  value       = github_repository.this.id
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.repo_id }
 }
 
 output "primary_language" {
   description = "The primary language used in the repository."
-  value       = github_repository.this.primary_language
+  value       = { for github_repository in github_repository.this : github_repository.name => github_repository.primary_language }
 }
 
 output "branch_protection" {
