f89dec4237040e53c698c3804a254f376b58e86f
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-04-15 09:06:41 -0400

    v1.0.21

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "modules_name" {
   description = "(Required) A list of modules name to published."
   type        = list(string)
   validation {
-    condition     = alltrue([for module_name in var.modules_name : can(regex("^terraform-[a-zA-Z--]+-[a-zA-Z--]+$", module_name))]) ? true : false
+    condition     = alltrue([for module_name in var.modules_name : can(regex("^terraform-[a-zA-Z]+-[a-zA-Z0-9--]+$", module_name))]) ? true : false
     error_message = "Module name must use a three-part name format like  `terraform-<PROVIDER>-<NAME>` and contain only letters and hypens."
   }
 }
